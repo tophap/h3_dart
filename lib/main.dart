99663b79f94f7b71d211a8a5c5d005c45d9a50fa
Razvan Cristian Lung <lung.razvan@gmail.com>
2019-08-23 14:46:55 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:h3_ffi/h3_ffi.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(geoToH3(
+          GeoCoord.degrees(
+            lat: 40.68942184369929,
+            lon: -74.04443139990863,
+          ),
+          10,
+        ).toRadixString(16)),
+      ),
+    );
+  }
+}
